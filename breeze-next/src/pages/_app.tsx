c7db675de6bc40a421c91b857243176865f832a2
phan_baninh <ninh.de@gmail.com>
2022-11-17 15:10:39 +0700

    first commit

---
+++ breeze-next/src/pages/_app.tsx
@@ -0,0 +1,5 @@
+import 'tailwindcss/tailwind.css'
+
+const App = ({ Component, pageProps }: { Component: React.ComponentClass<any>, pageProps: any }) => <Component {...pageProps} />
+
+export default App
